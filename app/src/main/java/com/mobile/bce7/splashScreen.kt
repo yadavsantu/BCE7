0863c534b47de8ea856ae73815dbda7e8b360c67
santu yadav <115726207+yadavsantu@users.noreply.github.com>
2025-05-21 22:06:40 +0545

    splash screen added

---
+++ app/src/main/java/com/mobile/bce7/splashScreen.kt
@@ -0,0 +1,19 @@
+package com.mobile.bce7
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
